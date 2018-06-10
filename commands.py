57ee68e5203d7674e07e4255d199f4c3bac2d97b
transistor <>
2018-06-10 04:23:47 -0600

    New implementation of embed.py
    Complete rewrite to take advantage of the Embed() class provided in
    discord.py
    
    See issue #3 (github:rolley-py/issues/3)

--- commands.py
+++ commands.py
@@ -1,11 +1,12 @@
 import discord
 from utils.config import ROLES
-from utils.embeds import Embed
-from discord.utils import get
+from utils.embeds import get_embeds
 from utils.perms import is_mod_or_admin
 
 
 async def help_cmd(bot, message):
+    """ Prints a help message in the channel """
+
     help_embed = discord.Embed(title='Help', type='rich', description="Commands: help, init", color=0xffffff)
     await bot.send_message(message.channel, embed=help_embed)
 
@@ -16,17 +17,15 @@ async def init(bot, message):
     else:
         await bot.send_message(message.channel, "Initiating...")
 
-        all_paired_embeds = Embed.create_embeds()
-        for i in range(0, len(all_paired_embeds)):
-            init_embed = discord.Embed(title=all_paired_embeds[i].title, type='rich',
-                                       description=all_paired_embeds[i].message, color=0xffffff)
-            init_message = await bot.send_message(message.channel, embed=init_embed)
+        all_embeds = get_embeds()
+        for emb in all_embeds:
+            init_msg = await bot.send_message(message.channel, embed=emb)
 
-            role_group = list(sorted(ROLES.keys()))[i]
-            curr_roles = ROLES[role_group]
-            for r, emoji in curr_roles.items():
-                reaction = get(bot.get_all_emojis(), name=emoji)
-                # if reaction is not a default Discord reaction
-                if reaction is None:
-                    reaction = emoji
-                await bot.add_reaction(init_message, reaction)
+            for group in ROLES.values():    # Retrives sub-dicts
+                for emoji in group.keys():
+                    reaction = discord.utils.get(bot.get_all_emojis(), name=emoji)
+
+                    # if reaction is not a default Discord reaction
+                    if reaction is None:
+                        reaction = emoji
+                    await bot.add_reaction(init_msg, reaction)
--- utils/embeds.py
+++ utils/embeds.py
@@ -1,15 +1,13 @@
+import discord
 from config import EMBEDS
 
+def _generate_embeds(data):
+    """ Returns a list of discord.Embed objects generated from the input data """
 
-class Embed(object):
-    def __init__(self, title="", description=""):
-        self.title = title
-        self.message = description
+    return [discord.Embed(title=t, description=d, type='rich', color=0xffffff) for t,d in data]
 
-    @staticmethod
-    def create_embeds():
-        title, message = 0, 1
-        all_paired_embeds = []
-        for i in range(len(EMBEDS)):
-            all_paired_embeds.append(Embed(EMBEDS[i][title], EMBEDS[i][message]))
-        return all_paired_embeds
+def get_embeds():
+    """ Directly returns the list of Embed objects """
+    
+    return _generate_embeds(EMBEDS)
+
